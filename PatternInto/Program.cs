81fc4945b3b53205cbaa3f9fa6da3f6fba43c1bf
mehedi <mehedisun@outlook.com>
2023-09-01 22:32:42 +0600

    added PatternIntro project

--- PatternInto/Program.cs
+++ PatternInto/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+3 Types of Design Pattern:
+
+# Creational - ( 5 Patterns)
+
+    > Abstract Factory
+    > Builder
+    > Factory Method
+    > Prototype
+    > Signleton
+    
+
+# Structural - ( 7 Patterns )
+
+    > Adapter
+    > Bridge
+    > Composite
+    > Decorator
+    > Facade
+    > Flyweight
+    > Proxy
+
+# Behaivour - ( 11 Patterns )
+
+    > Chain of Responsibility
+    > Command
+    > Interpreter
+    > Iterator
+    > Mediator
+    > Memento
+    > Observer
+    > State
+    > Strategy
+    > Template Method
+    > Visitor
+
+*/
