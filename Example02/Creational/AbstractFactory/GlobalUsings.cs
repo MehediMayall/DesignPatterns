171b2bed92d6d7828ca30fa23fdbaaf32d0eede8
mehedi <mehedisun@outlook.com>
2023-09-05 16:58:28 +0600

    updated

--- Example02/Creational/AbstractFactory/GlobalUsings.cs
+++ Example02/Creational/AbstractFactory/GlobalUsings.cs
@@ -2,3 +2,4 @@ global using System.Data;
 global using System.Data.Common;
 global using System.Data.SqlClient;
 
+global using AbstractFactory;
--- Example02/Creational/AbstractFactory/Program.cs
+++ Example02/Creational/AbstractFactory/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿/*
+Difference between Factory Method and Abstract Factory.
+
+Factory Method is a single method, and Abstract Factory is an abstract object.
+
+
+*/
+
+
+var buildMiniCarInIndia = CarFactory.BuildCar(Locations.INDIA, CarTypes.MINI);
+
+var buildLuxuryCarInGermany = CarFactory.BuildCar(Locations.GERMANY, CarTypes.LUXURY);
+
+var buildMicroCarInChina = CarFactory.BuildCar(Locations.CHINA, CarTypes.MICRO);
