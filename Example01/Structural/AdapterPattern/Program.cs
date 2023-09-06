afead80daf40d0f95c016c7ac72fd86b79b4b25b
mehedi <mehedisun@outlook.com>
2023-09-06 23:33:50 +0600

    Adapter Pattern

--- Example01/Structural/AdapterPattern/Program.cs
+++ Example01/Structural/AdapterPattern/Program.cs
@@ -1,4 +1,19 @@
-﻿using AdapterPattern;
+﻿/*
+Adapter is a structural design pattern that allows objects with incompatible interfaces
+to collaborate.
+
+Adapter is also known as Wrapper.
+An Adapter allows two incompatible interfaces to work together. Inteface may be incompatible, but the inner functionality should
+suit the need.
+
+The adapter design pattern allows incompatible classed to work together by converting the interface of a class into an 
+interface expected by the clients.
+
+*/
+
+
+
+using AdapterPattern;
 
 IPlayer player = new PlayerAdapter();
 
